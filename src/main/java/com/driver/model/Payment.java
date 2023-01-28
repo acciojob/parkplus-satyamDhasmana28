1b00006f58554ed1382f305d1c2649691ed89d34
Your Name <satyam.dhasmanaa@gmail.com>
2023-01-28 17:15:39 +0530

    test2

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -23,7 +23,7 @@ public class Payment {
         this.id = id;
     }
 
-    public Boolean getPaymentCompleted() {
+    public Boolean isPaymentCompleted() {
         return paymentCompleted;
     }
 
--- src/main/java/com/driver/model/Spot.java
+++ src/main/java/com/driver/model/Spot.java
@@ -42,7 +42,7 @@ public class Spot {
         this.pricePerHour = pricePerHour;
     }
 
-    public boolean isOccupied() {
+    public boolean getOccupied() {
         return occupied;
     }
 
